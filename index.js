e4fd057c8d066bda49ae0326c72dc10533ffe25b
AbhishekRangi <srinivarangi30@gmail.com>
2021-04-16 16:24:50 +0530

    a

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// const http =require('http');
+// const server = http.createServer(
+//     function f1(request,response)
+//     {
+//         response.write('Hello World\n');
+//         response.end();
+//     }
+// );
+// server.listen(9000,function()
+//     {   
+//         console.log('server is runnning at port 9000.....');
+//     }
+// );
 const http =require('http');
 const server = http.createServer(
     function f1(request,response)
