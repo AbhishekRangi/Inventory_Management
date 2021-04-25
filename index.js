ba92150f265378ba941fd0bd1e8531ec5a873203
AbhishekRangi <srinivarangi30@gmail.com>
2021-04-25 23:06:11 +0530

    public and view directories commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(
     function f1(request,response)
     {
         response.write('Hello World\n');
-        response.end();
+        response.end(); 
     }
 );
 server.listen(9000,function()
